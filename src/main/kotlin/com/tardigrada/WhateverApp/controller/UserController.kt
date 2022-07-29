60d78e7a0141f3297f2135f70859890b5c953eed
Cintulova <eliska.cintulova@capgemini.com>
2022-07-29 10:32:34 +0200

    exceptions added and working

--- src/main/kotlin/com/tardigrada/WhateverApp/controller/UserController.kt
+++ src/main/kotlin/com/tardigrada/WhateverApp/controller/UserController.kt
@@ -2,11 +2,21 @@ package com.tardigrada.WhateverApp.controller
 
 import com.tardigrada.WhateverApp.model.User
 import com.tardigrada.WhateverApp.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/users")
 class UserController(private val userService: UserService) {
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
+        ResponseEntity(e.message, HttpStatus.NOT_FOUND)
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleNotFound(e: IllegalArgumentException): ResponseEntity<String> =
+        ResponseEntity(e.message, HttpStatus.BAD_REQUEST)
 
     @GetMapping
     fun getUsers() = userService.getUsers()
--- src/main/kotlin/com/tardigrada/WhateverApp/service/UserService.kt
+++ src/main/kotlin/com/tardigrada/WhateverApp/service/UserService.kt
@@ -7,17 +7,35 @@ import org.springframework.stereotype.Service
 @Service
 class UserService(private val userRepository: UserRepository) {
 
-    fun saveUser(user: User) = userRepository.save(user)
-
-    fun getUsers() = userRepository.findAll()
-
-    fun getUserByUuid(userUuid: Int) = userRepository.findById(userUuid)
-
-    fun updateUserByUuid(user: User, userUuid: Int) {
-        deleteUserByUuid(userUuid)
-        saveUser(user)
+    fun saveUser(user: User): User {
+        if(getUsers().any { it.email == user.email }) {
+            throw java.lang.IllegalArgumentException("User with e-mail ${user.email} already exists.")
+        }
+        return userRepository.save(user)
     }
 
-    fun deleteUserByUuid(userUuid: Int) = userRepository.deleteById(userUuid)
+    fun getUsers(): MutableIterable<User> {return userRepository.findAll() }
+
+    fun getUserByUuid(userUuid: Int): User {
+        if (userRepository.findById(userUuid).isEmpty) {
+            throw NoSuchElementException("Could not find user with id $userUuid")
+        }
+        return userRepository.findById(userUuid).get()
+    }
+
+    fun updateUserByUuid(user: User, userUuid: Int): User {
+        if (userRepository.findById(userUuid).isEmpty) {
+            throw NoSuchElementException("Could not find user with id $userUuid")
+        }
+        deleteUserByUuid(userUuid)
+        return saveUser(user)
+    }
+
+    fun deleteUserByUuid(userUuid: Int): Unit {
+        if (userRepository.findById(userUuid).isEmpty) {
+            throw NoSuchElementException("Could not find user with id $userUuid")
+        }
+        return userRepository.deleteById(userUuid)
+    }
 
 }
